a2442d379d3675f3a258ef24d88a2886f52fcb10
https://bdoctor@github.com <miner49er@ps-ax.com>
2017-12-24 10:47:47 -0700

    syslog additions

--- wolf/log.c
+++ wolf/log.c
@@ -3,6 +3,12 @@
 #include <stdarg.h>
 #include <pthread.h>
 #include "minerlog.h"
+#include <syslog.h>
+#include <unistd.h>
+#include <string.h>
+#include <inttypes.h>
+#include <alloca.h>
+
 
 pthread_mutex_t LogMutex = PTHREAD_MUTEX_INITIALIZER;
 static uint32_t LogLevel = LOG_INVALID;
@@ -10,9 +16,15 @@ static uint32_t LogLevel = LOG_INVALID;
 void Log(uint32_t MsgLevel, char *Msg, ...)
 {
 	va_list args;
+	va_list args2;
+	char *syslogbuf;
+	int sysloglen;
+
 	time_t rawtime;
 	char timebuf[128];
+
 	struct tm *curtime;
+
 	
 	if(MsgLevel <= LogLevel && LogLevel != LOG_INVALID)
 	{
@@ -22,14 +34,22 @@ void Log(uint32_t MsgLevel, char *Msg, ...)
 		
 		pthread_mutex_lock(&LogMutex);
 		
-		printf(timebuf);
-		
 		va_start(args, Msg);
-		vprintf(Msg, args);
+
+		va_copy(args2, args);
+
+		sysloglen = vsnprintf(NULL, 0, Msg, args2) + 1;
+
+		va_end(args2);
+
+		syslogbuf = (char*) alloca(sysloglen);
+		if (vsnprintf(syslogbuf, sysloglen, Msg, args) >= 0)
+			syslog(LOG_INFO, "%s", syslogbuf);
+	
 		va_end(args);
-		putchar('\n');
 		
 		pthread_mutex_unlock(&LogMutex);
+		printf("%s\n", syslogbuf);
 	}
 	
 	return;
@@ -38,5 +58,6 @@ void Log(uint32_t MsgLevel, char *Msg, ...)
 void InitLogging(uint32_t DesiredLogLevel)
 {
 	LogLevel = DesiredLogLevel;
+	openlog("miner", LOG_PID|LOG_CONS, LOG_USER);
 	return;
 }
