1ad7e80f43f2207a52a2a84f1158674585f9a0cf
https://bdoctor@github.com <miner49er@ps-ax.com>
2017-12-27 20:54:27 -0700

    multiple cards

--- amdpmon.py
+++ amdpmon.py
@@ -1,48 +1,61 @@
 #!/usr/bin/python
-import sys, re
+import sys, re, os
 import syslog
 
-stat_file = '/sys/kernel/debug/dri/0/amdgpu_pm_info'
+
+adapters = '/sys/kernel/debug/dri'
+stat_file = '/sys/kernel/debug/dri/%s/amdgpu_pm_info'
 
 #/sys/kernel/debug/dri/0/amdgpu_pm_info
 
-lnfh = open(stat_file, 'r')
-lines = lnfh.readlines()
-lnfh.close()
-#print lines
+adapter_files = os.listdir(adapters)
 
-pwrRE = re.compile('(^.*)\((.*)\)$')
-powerout = False
-logMsg = ''
+for gpu in adapter_files:
 
-for l in lines:
-    l = l.strip()
-    l = l.lstrip()
-    #print l
-    if l == '': continue
-
-    if 'GFX Clocks and Power' in l:
-        powerout = True
-        continue
-
-    if 'GPU Load' in l:
-        (Toss,Load) = l.split(':')
-        powerout = False
-        continue
-
-    if 'GPU Temperature' in l:
-        (Toss,Temp) = l.split(':')
-        powerout = False
-        continue
-
-    if powerout:
-        match = pwrRE.search(l)
-        if match:
-            #print 'gotit', match.group(1), match.group(2)
-            #print '%10s\t\t%s' % (match.group(2), match.group(1))
-            logMsg += '%s:%s,' % (match.group(2), match.group(1).strip())
-        else:
-            print 'missed ', l
-
-toLog = '%s%s' % (logMsg,'GPUTemp:%s,GPULoad:%s' % (Temp,Load))
-syslog.syslog(toLog)
+    lines  = ''
+    logMsg = ''
+ 
+    try:
+        lnfh = open(stat_file % gpu, 'r')
+        lines = lnfh.readlines()
+        lnfh.close()
+        #print lines
+    except Exception, e:
+        pass
+        #print 'error: %s' % e
+    
+    pwrRE = re.compile('(^.*)\((.*)\)$')
+    powerout = False
+    logMsg = ''
+    
+    for l in lines:
+        l = l.strip()
+        l = l.lstrip()
+        #print l
+        if l == '': continue
+    
+        if 'GFX Clocks and Power' in l:
+            powerout = True
+            continue
+    
+        if 'GPU Load' in l:
+            (Toss,Load) = l.split(':')
+            powerout = False
+            continue
+    
+        if 'GPU Temperature' in l:
+            (Toss,Temp) = l.split(':')
+            powerout = False
+            continue
+    
+        if powerout:
+            match = pwrRE.search(l)
+            if match:
+                #print 'gotit', match.group(1), match.group(2)
+                #print '%10s\t\t%s' % (match.group(2), match.group(1))
+                logMsg += '%s:%s,' % (match.group(2), match.group(1).strip())
+            else:
+                print 'missed ', l
+    
+    toLog = '%s,%s%s' % ('Adapter:%s' % gpu,logMsg,'GPUTemp:%s,GPULoad:%s' % (Temp,Load))
+    syslog.syslog(toLog)
