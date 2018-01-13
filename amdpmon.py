30736a6459f4008a4997442823343bd6d033da12
bdoctor <brad.doctor@gmail.com>
2018-01-12 23:30:03 -0700

    track fan speed and value

--- amdpmon.py
+++ amdpmon.py
@@ -3,34 +3,16 @@ import sys, re, os
 import syslog
 import commands
 
-dead_strings = {}
+try:
+    debug = sys.argv[1]
+    debug = True
+except:
+    debug = False
 
-#Jan  9 09:16:43 miner00 kernel: [ 2160.191729] INFO: task nsgpucnminer:2812 blocked for more than 120 seconds.
-#Jan  9 09:16:43 miner00 kernel: [ 2160.191801]       Tainted: G           OE   4.4.0-104-generic #127-Ubuntu
-#Jan  9 09:16:43 miner00 kernel: [ 2160.191861] "echo 0 > /proc/sys/kernel/hung_task_timeout_secs" disables this message.
-#Jan  9 09:16:43 miner00 kernel: [ 2160.191930] nsgpucnminer    D ffff88019398b850     0  2812      1 0x00000002
-#Jan  9 09:16:43 miner00 kernel: [ 2160.191939]  ffff88019398b850 ffff88019398b830 ffffffff81e11500 ffff8801b5aca700
-#Jan  9 09:16:43 miner00 kernel: [ 2160.191945]  ffff88019398c000 ffff88019398b950 ffff880196e578b0 0000000000000001
-
-dead_strings['nsgpucminer'] = 'Claymore hung on GPU failed'
-dead_strings['amd_sched_entity_push_job'] = 'GPU died'
-
-restart_script = '/home/bdoctor/claymore/restart.sh'
-
-(s,o) = commands.getstatusoutput('/bin/dmesg')
-
-this = o.split() 
-
-for line in this:
-    #print line
-    for string in dead_strings:
-        if string in line:
-            syslog.syslog('GPU has died: (%s) -- REBOOTING' % line)
-            (s,o) = commands.getstatusoutput(restart_script)
-            print o
-
-adapters = '/sys/kernel/debug/dri'
+adapters  = '/sys/kernel/debug/dri'
 stat_file = '/sys/kernel/debug/dri/%s/amdgpu_pm_info'
+fan_value = '/sys/class/hwmon/hwmon%s/pwm1'
+fan_rpm   = '/sys/class/hwmon/hwmon%s/fan1_input'
 
 #/sys/kernel/debug/dri/0/amdgpu_pm_info
 
@@ -45,10 +27,17 @@ for gpu in adapter_files:
         lnfh = open(stat_file % gpu, 'r')
         lines = lnfh.readlines()
         lnfh.close()
+	fvfh = open(fan_value % gpu, 'r')
+        fvlines = fvfh.readlines()
+        fvfh.close()
+
+	frpmfh    = open(fan_rpm % gpu, 'r')
+        frpmlines = frpmfh.readlines()
+        frpmfh.close()
         #print lines
     except Exception, e:
+	if debug: print 'error: %s' % e
         pass
-        #print 'error: %s' % e
     
     pwrRE = re.compile('(^.*)\((.*)\)$')
     powerout = False
@@ -79,12 +68,12 @@ for gpu in adapter_files:
         if powerout:
             match = pwrRE.search(l)
             if match:
-                #print 'gotit', match.group(1), match.group(2)
-                #print '%10s\t\t%s' % (match.group(2), match.group(1))
                 logMsg += '%s:%s,' % (match.group(2), match.group(1).strip())
             else:
                 print 'missed ', l
     
-    toLog = '%s,%s%s' % ('Adapter:%s' % gpu,logMsg,'GPUTemp:%s,GPULoad:%s' % (Temp,Load))
+    toLog = '%s,%s%s' % ('Adapter:%s' % gpu,logMsg,'GPUTemp:%s, GPUFanRPM:%s, GPUFanInput:%s, GPULoad:%s' % (Temp,frpmlines[0].strip(),fvlines[0].strip(),Load))
     if Temp and Load:
+        if debug:
+            print toLog
         syslog.syslog(toLog)
