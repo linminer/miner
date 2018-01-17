a385a740eca4a7b8e4e0a9245b4758c013dc58a5
bdoctor <brad.doctor@gmail.com>
2018-01-17 07:31:52 -0700

    put the reboot logic back

--- amdpmon.py
+++ amdpmon.py
@@ -14,6 +14,36 @@ stat_file = '/sys/kernel/debug/dri/%s/amdgpu_pm_info'
 fan_value = '/sys/class/hwmon/hwmon%s/pwm1'
 fan_rpm   = '/sys/class/hwmon/hwmon%s/fan1_input'
 
+dead_strings = {}
+
+#Jan  9 09:16:43 miner00 kernel: [ 2160.191729] INFO: task nsgpucnminer:2812 blocked for more than 120 seconds.
+#Jan  9 09:16:43 miner00 kernel: [ 2160.191801]       Tainted: G           OE   4.4.0-104-generic #127-Ubuntu
+#Jan  9 09:16:43 miner00 kernel: [ 2160.191861] "echo 0 > /proc/sys/kernel/hung_task_timeout_secs" disables this message.
+#Jan  9 09:16:43 miner00 kernel: [ 2160.191930] nsgpucnminer    D ffff88019398b850     0  2812      1 0x00000002
+#Jan  9 09:16:43 miner00 kernel: [ 2160.191939]  ffff88019398b850 ffff88019398b830 ffffffff81e11500 ffff8801b5aca700
+#Jan  9 09:16:43 miner00 kernel: [ 2160.191945]  ffff88019398c000 ffff88019398b950 ffff880196e578b0 0000000000000001
+
+dead_strings['segfault at'] = 'detected segfault!'
+dead_strings['amd_sched_entity_push_job'] = 'GPU died'
+
+restart_script = '/home/bdoctor/miner/restart.sh'
+
+(s,o) = commands.getstatusoutput('/bin/dmesg')
+
+this = o.split('\n')
+
+for line in this:
+    #print line
+    for string in dead_strings:
+        if string in line:
+            if debug:
+                print 'reboot!! ', line
+
+            syslog.syslog('GPU has died: (%s) -- REBOOTING' % line)
+            (s,o) = commands.getstatusoutput(restart_script)
+            print o
+
+
 #/sys/kernel/debug/dri/0/amdgpu_pm_info
 
 adapter_files = os.listdir(adapters)
